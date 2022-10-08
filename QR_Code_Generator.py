8863eb73535a7a677427d887a4bdc591cde2dba2
Chirag Chakraborty <chiragchakraborty48@gmail.com>
2022-10-08 17:58:20 +0530

    Returned the function

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -5,7 +5,8 @@ def generate_QR(URL):
     qr_img = qrcode.make(URL)
     fileNum = str(random.randint(0, 999999))
     qr_img.save("QR_" + fileNum + ".jpg")
-    print('QR Code for your link has been generated and saved successfully.')
+    print("QR Code for your link has been generated successfully.")
+    return qr_img
 
 link = input("Please enter a text or URL: ")
 generate_QR(link)
